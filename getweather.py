dc47248164fc173a5aa280eebcb367e1b8554c5f
jangwalia <41533082+jangwalia@users.noreply.github.com>
2020-07-26 16:20:38 -0700

    Delete getweather.py

--- getweather.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def showweather():
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=surrey,CA&units=metric&appid=20e782bf2131b3ae1381f0780e79eb7d'
-    r = requests.get(url)
-    data = r.json()
-    return(data)
